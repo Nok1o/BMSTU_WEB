8b15a804e4b826424248aebce843c90edc914ed3
mogilinns <nikitamogilin39@gmail.com>
2025-10-25 21:37:49 +0300

    starting new lab.

--- deploy/sql/init.sql
+++ deploy/sql/init.sql
@@ -340,3 +340,28 @@ CREATE TRIGGER trg_update_chat_updated_at
     FOR EACH ROW
 EXECUTE FUNCTION update_chat_updated_at();
 
+
+-- readonly role
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'readonly') THEN
+CREATE ROLE readonly;
+END IF;
+END
+$$;
+
+-- Даём права на подключение к БД
+GRANT CONNECT ON DATABASE quickflow_db TO readonly;
+
+-- Даём USAGE на схему
+GRANT USAGE ON SCHEMA public TO readonly;
+
+-- Даём SELECT на все существующие таблицы
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
+
+-- Даём SELECT на все будущие таблицы
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;
+
+-- Опционально: разрешаем SELECT на последовательности (если используются SERIAL)
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO readonly;
